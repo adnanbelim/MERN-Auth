3deca6ef67386fcb14a6cc3fc90245e43cf3e358
Mohammed Adnan Belim <belimadnan111@gmail.com>
2024-12-28 23:42:42 +0530

    MERN Auth Web App

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 require('dotenv').config();
@@ -38,44 +37,3 @@ if (process.env.NODE_ENV !== 'production') {
         console.log(`Server running on PORT ${PORT}`);
     });
 }
-=======
-const express = require('express');
-const app = express();
-require('dotenv').config();
-require('./models/db.config');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./routes/AuthRouter');
-const ProductRouter = require('./routes/ProductRouter');
-const serverless = require('serverless-http');  // Import serverless-http
-
-const PORT = process.env.PORT || 8000;
-
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.options('*', cors());  // Allow all origins
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Backend works successfully');
-});
-
-app.get('/ping', (req, res) => {
-    res.send('pong');
-});
-
-// Auth and Product Routes
-app.use('/auth', AuthRouter);
-app.use('/products', ProductRouter);
-
-// Vercel serverless export
-module.exports.handler = serverless(app);  // Export the handler
-
-// Local server start (for development)
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on PORT ${PORT}`);
-    });
-}
->>>>>>> 4ff2ba754b573544ffbecc53f57d3d7034704018
